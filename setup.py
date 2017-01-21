4675442f96f48ff357c82dd3f7d75f098e5acbd4
Goncalo Santos <goncalo@reincubate.com>
2017-01-21 18:17:12 +0000

    make it downloadable through pip

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="Multi-Purpose data_structures in python",
+    version="0.0.1",
+    author="Goncalo Silva Santos",
+    author_email="goncalopvssantos@gmail.com",
+    description=("A variaty of multipurpose data structures implemented in python"),
+    license="BSD",
+    keywords="python data structures list queue binary search tree graph",
+    url="https://github.com/goncalossantos/data_strutures",
+    packages=['trees'],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
